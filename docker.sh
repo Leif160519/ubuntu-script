b67d1619f2f429856f156931a62132f586440a94
luofei1 <fei1.luo@genscript.com>
2020-07-13 13:49:02 +0800

    docker脚本新增修改docker网卡网段的内容

--- docker.sh
+++ docker.sh
@@ -6,6 +6,17 @@ cat <<EOF > /etc/docker/daemon.json
 "registry-mirrors": ["https://b9pmyelo.mirror.aliyuncs.com"]
 }
 EOF
+
+# 若想更换docker网卡的网段，则json内容如下
+#{
+#"default-address-pools": [
+#	{
+#		"base": "198.18.0.0/16",
+#		"size": 24
+#		}
+#	],
+#  "registry-mirrors": ["https://b9pmyelo.mirror.aliyuncs.com"]
+#}
 # 重启docker
 systemctl restart docker
 # 设置docker开机自启动
