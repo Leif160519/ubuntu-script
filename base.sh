2f57d400af5d4b8508cfb75c3ba366a83ba86bef
luofei1 <fei1.luo@genscript.com>
2020-07-21 14:23:07 +0800

    新增lrzsz文件传输工具

--- base.sh
+++ base.sh
@@ -72,6 +72,9 @@ echo -e "\033[1;32m  安装npm \033[0m"
 apt-get -y install npm
 echo -e "\033[1;32m  安装pv \033[0m"
 apt-get -y install pv
+echo -e "\033[1;32m  安装lrzsz \033[0m"
+apt-get -y install lrzsz
+
 
 # 设置SSH
 echo -e '\033[1;31m 关闭SSH DNS反向解析和GSSAPI的用户认证 \033[0m'
